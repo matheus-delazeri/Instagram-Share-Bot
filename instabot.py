4295f64f804560aa7dd51b51b98ade110ec008a3
Matheus Delazeri <matheusdelazeris@gmail.com>
2020-12-06 01:14:52 -0300

    Fixed a few bugs - Not opening link in Chrome fixed - The speed that the post is sent was increased

--- instabot.py
+++ instabot.py
@@ -7,35 +7,46 @@ from tkinter import messagebox
 class InstaBot:
     driver = None
     def __init__(self, username, pw, link, browser):
-        driver = self._get_browser(browser)
-        driver.get('https://www.instagram.com')
-        sleep(3)
-        login_field = driver.find_element_by_xpath("//input[@name=\"username\"]")\
-            .send_keys(username)
-        pw_field = driver.find_element_by_xpath("//input[@name=\"password\"]")\
-            .send_keys(pw)
-        btn_login = driver.find_element_by_xpath('//button[@type="submit"]')\
-            .click()
-        sleep(7)
-        driver.get(link)
-        while():
-            self._share_post(driver)
-        driver.quit()
+        if(username != "" and pw != "" and link != ""):
+            share_time = 7
+            driver = self._get_browser(browser)
+            driver.get('https://www.instagram.com')
+            sleep(3)
+            login_field = driver.find_element_by_xpath("//input[@name=\"username\"]")\
+                .send_keys(username)
+            pw_field = driver.find_element_by_xpath("//input[@name=\"password\"]")\
+                .send_keys(pw)
+            sleep(1)
+            btn_login = driver.find_element_by_xpath('//button[@type="submit"]')\
+                .click()
+            sleep(2)
+            try:
+                driver.find_element_by_id('slfErrorAlert')
+                messagebox.showwarning(title="Error", message="It seems your credentials are wrong! Try again")
+                driver.close()
+            except:
+                sleep(7)
+                driver.get(link)
+                while(True):
+                    self._share_post(driver, share_time)
+                    share_time = 1
+        else:
+            messagebox.showwarning(title="Error", message="All fields must be filled!")
     
-    def _share_post(self, driver):
+    def _share_post(self, driver, share_time):
         btn_share = driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[1]/button")\
             .click()
         sleep(1)
         btn_direct = driver.find_element_by_xpath("/html/body/div[5]/div/div/div/div[2]/div/div[1]")\
             .click()
-        sleep(7)
-        for x in range(2, 8):
-            btn_contacts = driver.find_element_by_xpath("/html/body/div[6]/div/div/div[2]/div[2]/div[%i]/div/div[3]/button" % x)\
+        sleep(share_time)
+        for x in range(2, 7):                       
+            btn_contacts = driver.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/div[2]/div[%i]/div/div[3]/button" % x)\
                 .click()
-            sleep(3)
-        btn_send = driver.find_element_by_xpath("/html/body/div[6]/div/div/div[1]/div/div[2]/div/button")\
+            sleep(1)
+        btn_send = driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/div/div[2]/div/button")\
             .click()
-        print("Foto enviada com sucesso!")
+        print("Post successufuly sent!")
         sleep(1)
 
     def _get_browser(self, browser):
